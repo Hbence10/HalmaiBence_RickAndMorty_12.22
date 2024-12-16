3ba4da3a84d5e10151c00bb807b2f757b6e3592e
Halmai Bence <93594678+Hbence10@users.noreply.github.com>
2024-12-16 17:45:08 +0100

    day 2

--- alap/scripts/characters.ts
+++ alap/scripts/characters.ts
@@ -13,19 +13,23 @@ class Character{
     ){}
 }
 
-const characterContainer : HTMLDivElement = document.getElementById("characterContainer") as HTMLDivElement
-const searchInput : HTMLInputElement = document.getElementById("searchInput") as HTMLInputElement
-const buttonRow : HTMLDivElement = document.getElementById("buttonRow") as HTMLDivElement
-const header : HTMLDivElement = document.getElementById("header") as HTMLDivElement
+const characterContainer : HTMLDivElement = document.getElementById("characterContainer") as HTMLDivElement;
+const episodeContainer : HTMLDivElement = document.getElementById("episodeContainer") as HTMLDivElement;
+const searchInput : HTMLInputElement = document.getElementById("searchInput") as HTMLInputElement;
+const buttonRow : HTMLDivElement = document.getElementById("buttonRow") as HTMLDivElement;
+const sideBar : HTMLDivElement = document.getElementById("sideBar") as HTMLDivElement;
+const header : HTMLDivElement = document.getElementById("header") as HTMLDivElement;
 interface origin {name : string, url : string}
 interface location {name : string, url : string}
+let showEpisodes : boolean = false
 let showInput : boolean = false
+let pageList : Character[] = []
 
 // Karakterekhez tartozo script:
 async function getCharacters(pageNumber : number) : Promise<any>{
     let apiCall : Promise<any> = (await fetch(`https://rickandmortyapi.com/api/character/?page=${pageNumber}`)).json()
     let apiData : Promise<any> = await apiCall
-    const pageList : Character[] = []
+    pageList = []
 
     apiData["results"].forEach(element => {
         pageList.push(new Character(element.id, element.name, element.status, element.species, element.type, element.gender, element.origin, element.location, element.image, element.episode))
@@ -37,15 +41,13 @@ async function getCharacters(pageNumber : number) : Promise<any>{
 }
 
 function loadCharacters(pageList : Character[]){
-    while (characterContainer.hasChildNodes()){
-        characterContainer.removeChild(characterContainer.firstChild)
-    }
+    characterContainer.innerHTML = ""
 
     for(let i : number = 0; i < pageList.length; i+=3){
         const row : HTMLDivElement = document.createElement("div")
-        row.classList.add("row", "characterRow", "my-3")
+        row.classList.add("row", "characterRow", "my-lg-5")
         const splittedRow : Character[] = pageList.slice(i, i+3)
-
+        
         for (let j : number = 0; j < splittedRow.length; j++){
             const card : HTMLDivElement = document.createElement("div")
             card.classList.add("col-lg-4", "d-flex", "justify-content-center", "align-items-center")
@@ -54,8 +56,11 @@ function loadCharacters(pageList : Character[]){
                     <img src="${splittedRow[j].imageLink}" class="card-img-top img-fluid" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">${splittedRow[j].name}</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <ul>
+                            <li><span class="fw-bold">Status: </span> ${splittedRow[j].status}</li>
+                            <li><span class="fw-bold">Gender: </span> ${splittedRow[j].gender}</li>
+                        </ul>
+                        <button class="btn btn-outline-dark" onclick="selectCharacter(${i+j})">Go somewhere</button>
                     </div>
                 </div>
             `
@@ -67,18 +72,78 @@ function loadCharacters(pageList : Character[]){
     }
 }
 
-function showSearchInput(){
+function selectCharacter(index : number){
+    const wantedCharacter : Character = pageList[index]
+    loadEpisodeList(wantedCharacter.episodeList);
+    sideBar.style.display = ""
+    sideBar.classList.add("showSideBar")
+    sideBar.classList.remove("hideSideBar")
+
+    console.log(wantedCharacter);
+
+    (document.getElementById("selectedImg") as HTMLImageElement).src = wantedCharacter.imageLink;
+    (document.getElementById("selectedName") as HTMLTitleElement).innerHTML = wantedCharacter.name;
+    
+    (document.getElementById("status") as HTMLSpanElement).innerHTML = wantedCharacter.status;
+    (document.getElementById("species") as HTMLSpanElement).innerHTML = wantedCharacter.species;
+    (document.getElementById("gender") as HTMLSpanElement).innerHTML = wantedCharacter.gender;
+    (document.getElementById("type") as HTMLSpanElement).innerHTML = wantedCharacter.type == "" ? "-" : wantedCharacter.type;
+
+    (document.getElementById("origin") as HTMLLinkElement).innerHTML = wantedCharacter.origin.name;
+    (document.getElementById("origin") as HTMLLinkElement).href = wantedCharacter.origin.url;
+
+    (document.getElementById("location") as HTMLLinkElement).innerHTML = wantedCharacter.location.name;
+    (document.getElementById("location") as HTMLLinkElement).href = wantedCharacter.location.url;
+}
+
+async function loadEpisodeList(episodeList : string[]){
+    episodeContainer.innerHTML = ""
+
+    for (let i : number = 0; i < episodeList.length; i++){
+        let apiCall : Promise<any> = (await fetch(episodeList[i])).json()
+        let apiData : Promise<any> = await apiCall
+
+        episodeContainer.innerHTML += `
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h5>${apiData["name"]}</h5>
+                    </div>
+                </div>
+            </div>
+        `        
+    }
+}
+
+// Egyeb dolgok:
+function showSearchInput() {
     showInput = !showInput
-    if (showInput){
+    if (showInput) {
         searchInput.style.display = ""
         header.classList.remove("hideInput")
         header.classList.add("showInput")
-    } else{
+    } else {
         searchInput.style.display = "none"
         header.classList.remove("showInput")
         header.classList.add("hideInput")
     }
 }
+
+function closeSideBar(){
+    sideBar.classList.remove("showSideBar")
+    sideBar.classList.add("hideSideBar")
+}
+
+function showEpisodeList(){
+    showEpisodes = !showEpisodes
+    if (showEpisodes){
+        episodeContainer.style.display = ""
+    } else{
+        episodeContainer.style.display = "none"
+    }
+}
+
+
 
 document.addEventListener("DOMContentLoaded", () =>{
     getCharacters(1)
@@ -87,5 +152,11 @@ document.addEventListener("DOMContentLoaded", () =>{
         buttonRow.innerHTML += `<button class='btn btn-outline-dark mx-2' onclick="getCharacters(${i})">${i}</button>`
     }
 
-    buttonRow.innerHTML += "<button class='btn btn-outline-dark mx-2' onclick='getCharacters(13)'>13</button>"
+    buttonRow.innerHTML += "<button class='btn btn-outline-dark mx-2' onclick='getCharacters(13)'>42</button>"
+})
+
+document.addEventListener("keydown", (event) => {
+    if (event.key === "Escape"){
+        closeSideBar()   
+    }
 })
--- alap/scripts/javascripts/characters.js
+++ alap/scripts/javascripts/characters.js
@@ -23,15 +23,19 @@ class Character {
     }
 }
 const characterContainer = document.getElementById("characterContainer");
+const episodeContainer = document.getElementById("episodeContainer");
 const searchInput = document.getElementById("searchInput");
 const buttonRow = document.getElementById("buttonRow");
+const sideBar = document.getElementById("sideBar");
 const header = document.getElementById("header");
+let showEpisodes = false;
 let showInput = false;
+let pageList = [];
 // Karakterekhez tartozo script:
 async function getCharacters(pageNumber) {
     let apiCall = (await fetch(`https://rickandmortyapi.com/api/character/?page=${pageNumber}`)).json();
     let apiData = await apiCall;
-    const pageList = [];
+    pageList = [];
     apiData["results"].forEach(element => {
         pageList.push(new Character(element.id, element.name, element.status, element.species, element.type, element.gender, element.origin, element.location, element.image, element.episode));
     });
@@ -39,12 +43,10 @@ async function getCharacters(pageNumber) {
     return apiCall;
 }
 function loadCharacters(pageList) {
-    while (characterContainer.hasChildNodes()) {
-        characterContainer.removeChild(characterContainer.firstChild);
-    }
+    characterContainer.innerHTML = "";
     for (let i = 0; i < pageList.length; i += 3) {
         const row = document.createElement("div");
-        row.classList.add("row", "characterRow", "my-3");
+        row.classList.add("row", "characterRow", "my-lg-5");
         const splittedRow = pageList.slice(i, i + 3);
         for (let j = 0; j < splittedRow.length; j++) {
             const card = document.createElement("div");
@@ -54,8 +56,11 @@ function loadCharacters(pageList) {
                     <img src="${splittedRow[j].imageLink}" class="card-img-top img-fluid" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">${splittedRow[j].name}</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <ul>
+                            <li><span class="fw-bold">Status: </span> ${splittedRow[j].status}</li>
+                            <li><span class="fw-bold">Gender: </span> ${splittedRow[j].gender}</li>
+                        </ul>
+                        <button class="btn btn-outline-dark" onclick="selectCharacter(${i + j})">Go somewhere</button>
                     </div>
                 </div>
             `;
@@ -65,6 +70,41 @@ function loadCharacters(pageList) {
         characterContainer.appendChild(row);
     }
 }
+function selectCharacter(index) {
+    const wantedCharacter = pageList[index];
+    loadEpisodeList(wantedCharacter.episodeList);
+    sideBar.style.display = "";
+    sideBar.classList.add("showSideBar");
+    sideBar.classList.remove("hideSideBar");
+    console.log(wantedCharacter);
+    document.getElementById("selectedImg").src = wantedCharacter.imageLink;
+    document.getElementById("selectedName").innerHTML = wantedCharacter.name;
+    document.getElementById("status").innerHTML = wantedCharacter.status;
+    document.getElementById("species").innerHTML = wantedCharacter.species;
+    document.getElementById("gender").innerHTML = wantedCharacter.gender;
+    document.getElementById("type").innerHTML = wantedCharacter.type == "" ? "-" : wantedCharacter.type;
+    document.getElementById("origin").innerHTML = wantedCharacter.origin.name;
+    document.getElementById("origin").href = wantedCharacter.origin.url;
+    document.getElementById("location").innerHTML = wantedCharacter.location.name;
+    document.getElementById("location").href = wantedCharacter.location.url;
+}
+async function loadEpisodeList(episodeList) {
+    episodeContainer.innerHTML = "";
+    for (let i = 0; i < episodeList.length; i++) {
+        let apiCall = (await fetch(episodeList[i])).json();
+        let apiData = await apiCall;
+        episodeContainer.innerHTML += `
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h5>${apiData["name"]}</h5>
+                    </div>
+                </div>
+            </div>
+        `;
+    }
+}
+// Egyeb dolgok:
 function showSearchInput() {
     showInput = !showInput;
     if (showInput) {
@@ -78,10 +118,28 @@ function showSearchInput() {
         header.classList.add("hideInput");
     }
 }
+function closeSideBar() {
+    sideBar.classList.remove("showSideBar");
+    sideBar.classList.add("hideSideBar");
+}
+function showEpisodeList() {
+    showEpisodes = !showEpisodes;
+    if (showEpisodes) {
+        episodeContainer.style.display = "";
+    }
+    else {
+        episodeContainer.style.display = "none";
+    }
+}
 document.addEventListener("DOMContentLoaded", () => {
     getCharacters(1);
     for (let i = 1; i <= 12; i++) {
         buttonRow.innerHTML += `<button class='btn btn-outline-dark mx-2' onclick="getCharacters(${i})">${i}</button>`;
     }
-    buttonRow.innerHTML += "<button class='btn btn-outline-dark mx-2' onclick='getCharacters(13)'>13</button>";
+    buttonRow.innerHTML += "<button class='btn btn-outline-dark mx-2' onclick='getCharacters(13)'>42</button>";
+});
+document.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+        closeSideBar();
+    }
 });
